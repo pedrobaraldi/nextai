710beb415a7426e3df2714f4609ac4bd1e545b13
Mateus Fattori <83764798+MateusFattori@users.noreply.github.com>
2024-04-12 11:32:54 -0300

    Add files via upload

---
+++ src/main/java/br/com/fiap/netxai/model/Produto.java
@@ -0,0 +1,23 @@
+package br.com.fiap.netxai.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+@Entity
+public class Produto {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "{produto.nome.not.blank}")
+    private String nome;
+    
+    private String categoria;
+
+    private Float valor;
+}
