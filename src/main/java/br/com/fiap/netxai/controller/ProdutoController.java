89da55975cfa64b8d640053b4e650a941ac9d8d7
Pedro Baraldi Sá <125573228+pedrobaraldi@users.noreply.github.com>
2024-09-16 15:56:21 -0300

    Add files via upload

--- src/main/java/br/com/fiap/netxai/controller/ProdutoController.java
+++ src/main/java/br/com/fiap/netxai/controller/ProdutoController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import br.com.fiap.netxai.repository.ProdutoRepository;
 import lombok.extern.slf4j.Slf4j;
 
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("produto")
 @Slf4j
